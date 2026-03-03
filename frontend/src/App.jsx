7b96828767ae98ff487af4c9299a18a7119cd331
ram-kumar-184593 <ramkumar184593@gmail.com>
2026-03-03 14:11:20 +0530

    Remove Vite base path for Render deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/codeheal">
+      <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
     </TooltipProvider>
